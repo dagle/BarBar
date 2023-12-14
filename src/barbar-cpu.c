a7290fe23fd9bb534024ac81404f0ace17e49eb3
Per Odlund <per.odlund@gmail.com>
2023-12-14 20:36:43 +0100

    A print function

--- src/barbar-cpu.c
+++ src/barbar-cpu.c
@@ -40,18 +40,36 @@ G_DEFINE_TYPE(BarBarCpu, g_barbar_cpu, GTK_TYPE_WIDGET)
 
 static void g_barbar_cpu_constructed(GObject *object);
 
+static void g_barbar_cpu_set_interval(BarBarCpu *cpu, guint inteval) {
+  g_return_if_fail(BARBAR_IS_CPU(cpu));
+
+  cpu->interval = inteval;
+
+  g_object_notify_by_pspec(G_OBJECT(clock), cpu_props[inteval]);
+}
+
 static void g_barbar_cpu_set_property(GObject *object, guint property_id,
-                                      const GValue *value, GParamSpec *pspec) {}
+                                      const GValue *value, GParamSpec *pspec) {
+
+  BarBarCpu *cpu = BARBAR_CPU(object);
+
+  switch (property_id) {
+  case PROP_INTERVAL:
+    g_barbar_cpu_set_interval(cpu, g_value_get_uint(value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+  }
+}
 
 static void g_barbar_cpu_get_property(GObject *object, guint property_id,
                                       GValue *value, GParamSpec *pspec) {
   BarBarCpu *cpu = BARBAR_CPU(object);
 
   switch (property_id) {
-    //  case PROP_STATES:
-    // g_value_get_string(value);
-    //    // g_barbar_disk_set_path(disk, g_value_get_string(value));
-    //    break;
+  case PROP_INTERVAL:
+    g_value_set_uint(value, cpu->interval);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
   }
@@ -97,6 +115,7 @@ void g_barbar_cpu_start(BarBarCpu *cpu, gpointer data);
 static void g_barbar_cpu_init(BarBarCpu *self) {
   // BarBarCpu *cpu = BARBAR_CPU(self);
 
+  // g_barbar_cpu_set_interval();
   self->interval = DEFAULT_INTERVAL;
 
   self->label = gtk_label_new("");
@@ -105,11 +124,217 @@ static void g_barbar_cpu_init(BarBarCpu *self) {
   g_signal_connect(self, "map", G_CALLBACK(g_barbar_cpu_start), NULL);
 }
 
+static gboolean is_number(const char *str, int *i) {
+
+  char *endptr;
+  long result = strtol(str, &endptr, 10);
+
+  if (*endptr != '\0') {
+    return FALSE;
+  }
+
+  *i = result;
+  return TRUE;
+}
+
+GString *print_props(const gchar *format, GObject *object) {
+  if (format == NULL) {
+    return NULL;
+  }
+
+  GString *str = g_string_new("");
+
+  while (true) {
+    const char *begin = strchr(format, '{');
+    if (begin == NULL) {
+      g_string_append(str, format);
+      break;
+    }
+
+    if (begin != format) {
+      g_string_append_len(str, format, begin - format);
+    }
+
+    const char *end = strchr(begin, '}');
+
+    if (end == NULL) {
+      /* Wasn't actually a tag, copy as-is instead */
+      g_string_append_len(str, format, begin - format + 1);
+      format = begin + 1;
+      continue;
+    }
+
+    char property_name_and_arg[end - begin];
+    strncpy(property_name_and_arg, begin + 1, end - begin - 1);
+    property_name_and_arg[end - begin - 1] = '\0';
+    const char *property_name = NULL;
+    const char *property_arg = NULL;
+
+    {
+      char *saveptr;
+      property_name = strtok_r(property_name_and_arg, ":", &saveptr);
+
+      property_arg = strtok_r(NULL, ":", &saveptr);
+    }
+    enum {
+      FMT_DEFAULT,
+      FMT_HEX,
+      FMT_OCT,
+      FMT_PERCENT,
+      FMT_KBYTE,
+      FMT_MBYTE,
+      FMT_GBYTE,
+      FMT_KIBYTE,
+      FMT_MIBYTE,
+      FMT_GIBYTE,
+    } num_format = FMT_DEFAULT;
+
+    int digits = 0;
+    int decimals = 2;
+    gboolean zero_pad;
+    char *point = NULL;
+
+    if (property_arg == NULL) {
+    } else if (strcmp(property_arg, "hex") == 0) {
+      num_format = FMT_HEX;
+    } else if (strcmp(property_arg, "oct") == 0) {
+      num_format = FMT_OCT;
+    } else if (strcmp(property_arg, "%") == 0) {
+      num_format = FMT_PERCENT;
+    } else if (strcmp(property_arg, "kb") == 0) {
+      num_format = FMT_KBYTE;
+    } else if (strcmp(property_arg, "mb") == 0) {
+      num_format = FMT_MBYTE;
+    } else if (strcmp(property_arg, "gb") == 0) {
+      num_format = FMT_GBYTE;
+    } else if (strcmp(property_arg, "kib") == 0) {
+      num_format = FMT_KIBYTE;
+    } else if (strcmp(property_arg, "mib") == 0) {
+      num_format = FMT_MIBYTE;
+    } else if (strcmp(property_arg, "gib") == 0) {
+      num_format = FMT_GIBYTE;
+    } else if (is_number(property_arg, &digits)) {
+      zero_pad = property_arg[0] == '0';
+    } // i.e.: "{tag:03}"
+    else if ((point = strchr(property_arg, '.')) != NULL) {
+      *point = '\0';
+
+      const char *digits_str = property_arg;
+      const char *decimals_str = point + 1;
+
+      if (digits_str[0] != '\0') { // guards against i.e. "{tag:.3}"
+        if (!is_number(digits_str, &digits)) {
+          // LOG_WARN("tag `%s`: invalid field width formatter. Ignoring...",
+          //          tag_name);
+        }
+      }
+
+      if (decimals_str[0] != '\0') { // guards against i.e. "{tag:3.}"
+        if (!is_number(decimals_str, &decimals)) {
+          // LOG_WARN("tag `%s`: invalid decimals formatter. Ignoring...",
+          //          tag_name);
+        }
+      }
+      zero_pad = digits_str[0] == '0';
+    } else {
+      // LOG_WARN("invalid tag formatter: %s", tag_args[i]);
+    }
+
+    GParamSpec *param_spec =
+        g_object_class_find_property(G_OBJECT_GET_CLASS(object), property_name);
+    // GType type = G_PARAM_SPEC_VALUE_TYPE(param_spec);
+
+    GValue value = G_VALUE_INIT;
+    g_value_init(&value, G_PARAM_SPEC_VALUE_TYPE(param_spec));
+    g_object_get_property(object, property_name, &value);
+    // g_object_get_property(object, property_name, &value);
+    switch (num_format) {
+    case FMT_DEFAULT: {
+      if (param_spec) {
+        if (G_IS_PARAM_SPEC_CHAR(param_spec)) {
+        } else if (G_IS_PARAM_SPEC_BOOLEAN(param_spec)) {
+        } else if (G_IS_PARAM_SPEC_LONG(param_spec)) {
+          // G_IS_PARAM_SPEC_LONG
+        } else if (G_IS_PARAM_SPEC_DOUBLE(param_spec)) {
+          const char *fmt = zero_pad ? "%0*.*f" : "%*.*f";
+
+          guint num = g_value_get_double(&value);
+          g_string_append_printf(str, fmt, digits, decimals, num);
+        } else if (G_IS_PARAM_SPEC_UINT(param_spec)) {
+          const char *fmt = zero_pad ? "%0*ld" : "%*ld";
+          guint num = g_value_get_uint(&value);
+          g_string_append_printf(str, fmt, digits, num);
+
+          // gint default_value =
+          //     g_value_get_int(g_param_spec_get_default_value(param_spec));
+          // guint min_value = G_PARAM_SPEC_UINT(param_spec)->minimum;
+          // guint max_value = G_PARAM_SPEC_UINT(param_spec)->maximum;
+        } else if (G_IS_PARAM_SPEC_STRING(param_spec)) {
+          const char *s = g_value_get_string(&value);
+          g_string_append(str, s);
+        }
+      }
+      break;
+    }
+    case FMT_HEX:
+    case FMT_OCT: {
+      const char *fmt = num_format == FMT_HEX ? zero_pad ? "%0*lx" : "%*lx"
+                        : zero_pad            ? "%0*lo"
+                                              : "%*lo";
+      guint num = g_value_get_uint(&value);
+      g_string_append_printf(str, fmt, digits, num);
+      break;
+    }
+    case FMT_PERCENT: {
+      guint num = g_value_get_uint(&value);
+      guint min_value = G_PARAM_SPEC_UINT(param_spec)->minimum;
+      guint max_value = G_PARAM_SPEC_UINT(param_spec)->maximum;
+
+      const char *fmt = zero_pad ? "%0*lu" : "%*lu";
+      g_string_append_printf(str, fmt, digits,
+                             (num - min_value) * 100 / (max_value - min_value));
+      break;
+    }
+    case FMT_KBYTE:
+    case FMT_MBYTE:
+    case FMT_GBYTE:
+    case FMT_KIBYTE:
+    case FMT_MIBYTE:
+    case FMT_GIBYTE: {
+      long div = num_format == FMT_KBYTE    ? 1000
+                 : num_format == FMT_MBYTE  ? 1000 * 1000
+                 : num_format == FMT_GBYTE  ? 1000 * 1000 * 1000
+                 : num_format == FMT_KIBYTE ? 1024
+                 : num_format == FMT_MIBYTE ? 1024 * 1024
+                 : num_format == FMT_GIBYTE ? 1024 * 1024 * 1024
+                                            : 1;
+      if (G_IS_PARAM_SPEC_DOUBLE(param_spec)) {
+        const char *fmt = zero_pad ? "%0*.*f" : "%*.*f";
+        double num = g_value_get_double(&value);
+
+        g_string_append_printf(str, fmt, digits, decimals, num / (double)div);
+      } else if (G_IS_PARAM_SPEC_UINT(param_spec)) {
+        const char *fmt = zero_pad ? "%0*lu" : "%*lu";
+        guint num = g_value_get_uint(&value);
+
+        g_string_append_printf(str, fmt, digits, num / div);
+      }
+      break;
+    }
+    }
+    format = end + 1;
+  }
+  printf("%s\n", str->str);
+  return str;
+}
+
 static void g_barbar_cpu_constructed(GObject *object) {}
 
 static gboolean g_barbar_cpu_update(gpointer data) {
   BarBarCpu *self = BARBAR_CPU(data);
   // double load[1];
+  //
+  print_props("bepa l{interval}", G_OBJECT(self));
 
   double total, idle, percent;
 
--- src/barbar-network.c
+++ src/barbar-network.c
@@ -24,12 +24,16 @@ struct _BarBarNetwork {
   guint interval;
 
   guint source_id;
+
+  guint64 bytes_in;
+  guint64 bytes_out;
 };
 
 enum {
   PROP_0,
 
   PROP_INTERVAL,
+  PROP_INTERFACE,
 
   NUM_PROPERTIES,
 };
@@ -47,19 +51,54 @@ static GParamSpec *network_props[NUM_PROPERTIES] = {
     NULL,
 };
 
+static void g_barbar_network_set_interface(BarBarNetwork *network,
+                                           const char *interface) {
+  g_return_if_fail(BARBAR_IS_NETWORK(network));
+
+  g_free(network->interface);
+  network->interface = g_strdup(interface);
+
+  g_object_notify_by_pspec(G_OBJECT(network), network_props[PROP_INTERVAL]);
+}
+
+static void g_barbar_network_set_interval(BarBarNetwork *network,
+                                          guint interval) {
+  g_return_if_fail(BARBAR_IS_NETWORK(network));
+
+  network->interval = interval;
+  if (network->source_id > 0) {
+  }
+
+  g_object_notify_by_pspec(G_OBJECT(network), network_props[PROP_INTERVAL]);
+}
 static void g_barbar_network_set_property(GObject *object, guint property_id,
                                           const GValue *value,
-                                          GParamSpec *pspec) {}
+                                          GParamSpec *pspec) {
+
+  BarBarNetwork *network = BARBAR_NETWORK(object);
+  switch (property_id) {
+  case PROP_INTERFACE:
+    g_barbar_network_set_interface(network, g_value_get_string(value));
+    break;
+  case PROP_INTERVAL:
+    g_barbar_network_set_interval(network, g_value_get_uint(value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+  }
+}
 
 static void g_barbar_network_get_property(GObject *object, guint property_id,
                                           GValue *value, GParamSpec *pspec) {
   BarBarNetwork *network = BARBAR_NETWORK(object);
 
   switch (property_id) {
-    //  case PROP_STATES:
-    // g_value_get_string(value);
-    //    // g_barbar_disk_set_path(disk, g_value_get_string(value));
-    //    break;
+  case PROP_INTERFACE:
+    g_value_set_string(value, network->interface);
+    break;
+  case PROP_INTERVAL:
+    g_value_set_uint(value, network->interval);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
   }
@@ -74,6 +113,9 @@ static void g_barbar_network_class_init(BarBarNetworkClass *class) {
   network_props[PROP_INTERVAL] =
       g_param_spec_uint("interval", "Interval", "Interval in milli seconds", 0,
                         G_MAXUINT32, DEFAULT_INTERVAL, G_PARAM_READWRITE);
+  network_props[PROP_INTERFACE] = g_param_spec_string(
+      "interface", "Interface", "Interface to measure speed on", NULL,
+      G_PARAM_READWRITE);
   g_object_class_install_properties(gobject_class, NUM_PROPERTIES,
                                     network_props);
   gtk_widget_class_set_layout_manager_type(widget_class, GTK_TYPE_BOX_LAYOUT);
@@ -84,6 +126,8 @@ void g_barbar_network_start(BarBarNetwork *net, gpointer data);
 static void g_barbar_network_init(BarBarNetwork *self) {
   self->interval = DEFAULT_INTERVAL;
   self->label = gtk_label_new("");
+  self->bytes_out = 0;
+  self->bytes_in = 0;
 
   gtk_widget_set_parent(self->label, GTK_WIDGET(self));
 
@@ -107,42 +151,20 @@ static char *hwaddress_format_for_display(glibtop_netload *buf) {
 static gboolean g_barbar_network_update(gpointer data) {
   BarBarNetwork *net = BARBAR_NETWORK(data);
 
+  guint64 in_diff;
+  guint64 out_diff;
+
   glibtop_netload netload;
-  unsigned method, count, port;
+  // unsigned method, count, port;
+  // unsigned count;
   struct in_addr addr, subnet;
   char address_string[INET_ADDRSTRLEN], subnet_string[INET_ADDRSTRLEN];
   char address6_string[INET6_ADDRSTRLEN], prefix6_string[INET6_ADDRSTRLEN];
-  char *hwaddress_string;
-  char buffer[BUFSIZ];
+  // char *hwaddress_string;
 
-  count = PROFILE_COUNT;
-
-  // setlocale(LC_ALL, "");
-  // bindtextdomain(GETTEXT_PACKAGE, GTOPLOCALEDIR);
-  // textdomain(GETTEXT_PACKAGE);
-
-  // glibtop_init_r(&glibtop_global_server, 0, GLIBTOP_INIT_NO_OPEN);
-
-  // glibtop_get_parameter(GLIBTOP_PARAM_METHOD, &method, sizeof(method));
-
-  // printf("Method = %d\n", method);
-
-  // count = glibtop_get_parameter(GLIBTOP_PARAM_COMMAND, buffer, BUFSIZ);
-  // buffer[count] = 0;
-
-  // printf("Command = '%s'\n", buffer);
-
-  // count = glibtop_get_parameter(GLIBTOP_PARAM_HOST, buffer, BUFSIZ);
-  // buffer[count] = 0;
-
-  // glibtop_get_parameter(GLIBTOP_PARAM_PORT, &port, sizeof(port));
-
-  // printf("Host = '%s' - %u\n\n", buffer, port);
-
-  // glibtop_init_r(&glibtop_global_server, 0, 0);
-
-  // if (argc != 2)
-  //   g_error("Usage: %s interface", argv[0]);
+  if (net->interface == NULL) {
+    return G_SOURCE_REMOVE;
+  }
 
   glibtop_get_netload(&netload, net->interface);
 
@@ -154,35 +176,58 @@ static gboolean g_barbar_network_update(gpointer data) {
   inet_ntop(AF_INET6, netload.address6, address6_string, INET6_ADDRSTRLEN);
   inet_ntop(AF_INET6, netload.prefix6, prefix6_string, INET6_ADDRSTRLEN);
 
-  hwaddress_string = hwaddress_format_for_display(&netload);
+  // hwaddress_string = hwaddress_format_for_display(&netload);
 
-  printf("Network Load (0x%016llx):\n\n"
-         "\tInterface Flags:\t0x%016llx\n"
-         "\tAddress:\t\t0x%08x - %s\n"
-         "\tSubnet:\t\t\t0x%08x - %s\n\n"
-         "\tMTU:\t\t\t%d\n"
-         "\tCollisions:\t\t%" G_GUINT64_FORMAT "\n\n"
-         "\tPackets In:\t\t%" G_GUINT64_FORMAT "\n"
-         "\tPackets Out:\t\t%" G_GUINT64_FORMAT "\n"
-         "\tPackets Total:\t\t%" G_GUINT64_FORMAT "\n\n"
-         "\tBytes In:\t\t%" G_GUINT64_FORMAT "\n"
-         "\tBytes Out:\t\t%" G_GUINT64_FORMAT "\n"
-         "\tBytes Total:\t\t%" G_GUINT64_FORMAT "\n\n"
-         "\tErrors In:\t\t%" G_GUINT64_FORMAT "\n"
-         "\tErrors Out:\t\t%" G_GUINT64_FORMAT "\n"
-         "\tErrors Total:\t\t%" G_GUINT64_FORMAT "\n\n"
-         "\tAddress6:\t\t%s\n"
-         "\tPrefix6:\t\t%s\n"
-         "\tScope6:\t\t\t%#03x\n\n"
-         "\tHarware Address:\t%s\n\n",
-         (unsigned long long)netload.flags,
-         (unsigned long long)netload.if_flags, (guint32)netload.address,
-         address_string, (guint32)netload.subnet, subnet_string, netload.mtu,
-         netload.collisions, netload.packets_in, netload.packets_out,
-         netload.packets_total, netload.bytes_in, netload.bytes_out,
-         netload.bytes_total, netload.errors_in, netload.errors_out,
-         netload.errors_total, address6_string, prefix6_string,
-         (int)netload.scope6, hwaddress_string);
+  if (!net->bytes_in) {
+    net->bytes_in = netload.bytes_in;
+    net->bytes_out = netload.bytes_out;
+  }
+
+  in_diff = netload.bytes_in - net->bytes_in;
+  out_diff = netload.bytes_out - net->bytes_out;
+
+  net->bytes_in = netload.bytes_in;
+  net->bytes_out = netload.bytes_out;
+
+  guint tick = net->interval / 1000;
+
+  // printf("bytes in: %lu kb/s\n", in_diff / (1024 * (net->interval / 1000)));
+  // printf("bytes out: %lu kb/s\n", out_diff / (1024 * (net->interval /
+  // 1000)));
+
+  gchar *str =
+      g_strdup_printf("in: %lu kb/s | out: %lu kb/s", in_diff / (1024 * tick),
+                      out_diff / (1024 * tick));
+  gtk_label_set_label(GTK_LABEL(net->label), str);
+  g_free(str);
+
+  // printf("Network Load (0x%016llx):\n\n"
+  //        "\tInterface Flags:\t0x%016llx\n"
+  //        "\tAddress:\t\t0x%08x - %s\n"
+  //        "\tSubnet:\t\t\t0x%08x - %s\n\n"
+  //        "\tMTU:\t\t\t%d\n"
+  //        "\tCollisions:\t\t%" G_GUINT64_FORMAT "\n\n"
+  //        "\tPackets In:\t\t%" G_GUINT64_FORMAT "\n"
+  //        "\tPackets Out:\t\t%" G_GUINT64_FORMAT "\n"
+  //        "\tPackets Total:\t\t%" G_GUINT64_FORMAT "\n\n"
+  //        "\tBytes In:\t\t%" G_GUINT64_FORMAT "\n"
+  //        "\tBytes Out:\t\t%" G_GUINT64_FORMAT "\n"
+  //        "\tBytes Total:\t\t%" G_GUINT64_FORMAT "\n\n"
+  //        "\tErrors In:\t\t%" G_GUINT64_FORMAT "\n"
+  //        "\tErrors Out:\t\t%" G_GUINT64_FORMAT "\n"
+  //        "\tErrors Total:\t\t%" G_GUINT64_FORMAT "\n\n"
+  //        "\tAddress6:\t\t%s\n"
+  //        "\tPrefix6:\t\t%s\n"
+  //        "\tScope6:\t\t\t%#03x\n\n"
+  //        "\tHarware Address:\t%s\n\n",
+  //        (unsigned long long)netload.flags,
+  //        (unsigned long long)netload.if_flags, (guint32)netload.address,
+  //        address_string, (guint32)netload.subnet, subnet_string, netload.mtu,
+  //        netload.collisions, netload.packets_in, netload.packets_out,
+  //        netload.packets_total, netload.bytes_in, netload.bytes_out,
+  //        netload.bytes_total, netload.errors_in, netload.errors_out,
+  //        netload.errors_total, address6_string, prefix6_string,
+  //        (int)netload.scope6, hwaddress_string);
 
   // glibtop_close ();
 
