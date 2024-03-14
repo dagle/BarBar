93072585655bfc154309c21f48992aa1f41c6a8b
Per Odlund <per.odlund@gmail.com>
2024-03-14 18:21:14 +0100

    A bluetooth boilerplate

--- src/sensors/barbar-bluetooth.c
+++ src/sensors/barbar-bluetooth.c
@@ -4,17 +4,25 @@
 #include <string.h>
 
 /**
- * BarBarGameMode:
+ * BarBarBluetooth:
  *
- * A sensor to display gamer mode status
+ * A sensor for bluetooth
  *
  */
 struct _BarBarBluetooth {
   BarBarSensor parent_instance;
+
+  GDBusObjectManager *manager;
+
+  gboolean enabled;
 };
 
 enum {
   PROP_0,
+
+  PROP_DEVICES,
+  PROP_ENABLED,
+  PROP_CONNECTED,
 
   NUM_PROPERTIES,
 };
@@ -61,119 +69,109 @@ static void g_barbar_bluetooth_set_property(GObject *object, guint property_id,
 static void g_barbar_bluetooth_get_property(GObject *object, guint property_id,
                                             GValue *value, GParamSpec *pspec) {
 
-  BarBarBluetooth *mode = BARBAR_BLUETOOTH(object);
+  BarBarBluetooth *bt = BARBAR_BLUETOOTH(object);
   switch (property_id) {
   // case PROP_GAME_COUNT:
-  //   g_value_set_uint(value, mode->game_count);
+  //   // g_value_set_uint(value, mode->game_count);
   //   break;
-  // case PROP_ENABLED:
-  //   g_value_set_boolean(value, mode->game_count > 0);
-  //   break;
+  case PROP_ENABLED:
+    g_value_set_boolean(value, bt->enabled);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
   }
 }
 
-static void g_barbar_bluetooth_class_init(BarBarGameModeClass *class) {
+static void g_barbar_bluetooth_class_init(BarBarBluetoothClass *class) {
   GObjectClass *gobject_class = G_OBJECT_CLASS(class);
   BarBarSensorClass *sensor_class = BARBAR_SENSOR_CLASS(class);
 
-  sensor_class->start = g_barbar_game_mode_start;
+  sensor_class->start = g_barbar_bluetooth_start;
 
-  gobject_class->set_property = g_barbar_game_mode_set_property;
-  gobject_class->get_property = g_barbar_game_mode_get_property;
+  gobject_class->set_property = g_barbar_bluetooth_set_property;
+  gobject_class->get_property = g_barbar_bluetooth_get_property;
 
-  // /**
-  //  * BarBarGameMode:count:
-  //  *
-  //  * How many games are currently running.
-  //  *
-  //  */
-  // systemd_props[PROP_GAME_COUNT] = g_param_spec_uint(
-  //     "game-count", NULL, NULL, 0, G_MAXUINT, 0,
-  //     G_PARAM_READWRITE | G_PARAM_CONSTRUCT | G_PARAM_STATIC_STRINGS);
-  //
-  // /**
-  //  * BarBarGameMode:enabled:
-  //  *
-  //  * If we are running in game mode
-  //  */
-  // systemd_props[PROP_ENABLED] = g_param_spec_boolean(
-  //     "enabled", NULL, NULL, FALSE, G_PARAM_READABLE |
-  //     G_PARAM_STATIC_STRINGS);
+  /**
+   * BarBarBluetooth:devices:
+   *
+   * How many games are currently running.
+   *
+   */
+  systemd_props[PROP_DEVICES] =
+      g_param_spec_boxed("devices", NULL, NULL, G_TYPE_STRV,
+                         G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
 
-  // g_object_class_install_properties(gobject_class, NUM_PROPERTIES,
-  //                                   systemd_props);
+  /**
+   *
+   * BarBarBluetooth:enabled:
+   *
+   * If bluetooth is enabled
+   */
+  systemd_props[PROP_ENABLED] = g_param_spec_boolean(
+      "enabled", NULL, NULL, FALSE, G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+  /**
+   *
+   * BarBarBluetooth:connected:
+   *
+   * If we are connected to bluetooth
+   */
+  systemd_props[PROP_CONNECTED] =
+      g_param_spec_boolean("connected", NULL, NULL, FALSE,
+                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties(gobject_class, NUM_PROPERTIES,
+                                    systemd_props);
+}
+static void on_interface_proxy_properties_changed(
+    GDBusObjectManagerClient *manager, GDBusObjectProxy *object_proxy,
+    GDBusProxy *interface_proxy, GVariant *changed_properties,
+    const gchar *const *invalidated_properties, gpointer user_data) {}
+
+static void on_interface_added(GDBusObjectManager *manager, GDBusObject *object,
+                               GDBusInterface *interface, gpointer user_data) {
+
+  GDBusProxy *proxy = G_DBUS_PROXY(interface);
+
+  // add the device to list of devices
+
+  // if this is our first device and we are connected, set connected.
 }
 
-static void g_barbar_game_mode_init(BarBarBluetooth *mode) {}
+static void on_interface_removed(GDBusObjectManager *manager,
+                                 GDBusObject *object, GDBusInterface *interface,
+                                 gpointer user_data) {
 
-static void update_failed(GObject *object, GAsyncResult *res, gpointer data) {
+  GDBusProxy *proxy = G_DBUS_PROXY(interface);
+  // remove the interface from the list
+
+  // if this was our last device, set disconnected
+}
+
+static void g_barbar_bluetooth_init(BarBarBluetooth *mode) {}
+
+static void get_initial_state(BarBarBluetooth *bt) {}
+
+static void manager_cb(GObject *source_object, GAsyncResult *res,
+                       gpointer data) {
+  BarBarBluetooth *bt = BARBAR_BLUETOOTH(data);
   GError *error = NULL;
 
-  BarBarBluetooth *bt = BARBAR_BLUETOOTH(object);
+  bt->manager = g_dbus_object_manager_client_new_for_bus_finish(res, &error);
 
-  // GVariant *ret = g_dbus_proxy_call_finish(G_DBUS_PROXY(object), res,
-  // &error);
-  //
-  // if (error || !ret) {
-  //   g_printerr("Failed to get failed systemd-units: %s\n", error->message);
-  //   g_error_free(error);
-  // }
-  // guint failed;
-  // GVariant *container;
-  // g_variant_get(ret, "(v)", &container);
-  //
-  // if (!container) {
-  //   return;
-  // }
-  // const GVariantType *type = g_variant_get_type(container);
-  // if (g_variant_type_equal(type, G_VARIANT_TYPE_UINT32)) {
-  //   g_variant_get(container, "u", &failed);
-  //
-  //   // g_barbar_systemd_units_set_profile(units, failed);
-  // }
-  // g_variant_unref(container);
-  // g_variant_unref(ret);
+  get_initial_state(bt);
+
+  g_signal_connect(bt->manager, "interface-proxy-properties-changed",
+                   G_CALLBACK(on_interface_proxy_properties_changed), bt);
+  g_signal_connect(bt->manager, "interface-added",
+                   G_CALLBACK(on_interface_added), bt);
+  g_signal_connect(bt->manager, "interface-removed",
+                   G_CALLBACK(on_interface_removed), bt);
 }
 
-static void get_failed_units(BarBarBluetooth *units) {
-  // g_dbus_proxy_call(
-  //     units->proxy, "Get",
-  //     g_variant_new("(ss)", "com.feralinteractive.GameMode", "ClientCount"),
-  //     G_DBUS_CALL_FLAGS_NONE, -1, NULL, update_failed, units);
-}
-
-static void get_default_value(BarBarBluetooth *units) {
-  get_failed_units(units);
-}
-
-static void g_properties_changed(GDBusProxy *self, GVariant *changed_properties,
-                                 char **invalidated_properties, gpointer data) {
-  BarBarGameMode *mode = BARBAR_GAME_MODE(data);
-}
-
-static void mode_cb(GObject *object, GAsyncResult *res, gpointer data) {
-  BarBarGameMode *mode = BARBAR_GAME_MODE(data);
-  GError *error = NULL;
-
-  mode->proxy = g_dbus_proxy_new_for_bus_finish(res, &error);
-
-  if (error) {
-    g_printerr("Failed to setup game-mode: %s\n", error->message);
-    g_error_free(error);
-  }
-  get_default_value(mode);
-
-  g_signal_connect(mode->proxy, "g-properties-changed",
-                   G_CALLBACK(g_properties_changed), mode);
-}
-
-static void g_barbar_game_mode_start(BarBarSensor *sensor) {
-  BarBarGameMode *mode = BARBAR_GAME_MODE(sensor);
-
-  g_dbus_proxy_new_for_bus(
-      G_BUS_TYPE_SESSION, G_DBUS_PROXY_FLAGS_NONE, NULL,
-      "com.feralinteractive.GameMode", "/com/feralinteractive/GameMode",
-      "org.freedesktop.DBus.Properties", NULL, mode_cb, mode);
+static void g_barbar_bluetooth_start(BarBarSensor *sensor) {
+  BarBarBluetooth *bt = BARBAR_BLUETOOTH(sensor);
+  g_dbus_object_manager_client_new_for_bus(
+      G_BUS_TYPE_SESSION, G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_DO_NOT_AUTO_START,
+      "org.bluez", "/", NULL, NULL, NULL, NULL, manager_cb, bt);
 }
